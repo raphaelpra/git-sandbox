7519b40326264bd45d5d37f3ec071536ad26a124
raphael <raphael.prasquier@imagine-app.fr>
2023-09-21 11:45:51 +0200

    Message en allemand

--- world.py
+++ world.py
@@ -2,8 +2,8 @@ print("message sur main")
 
 print("Bonjour tout le monde")
 
-print("Bonjour Vscode - Como estas")
-print("Bonjour Vscode - Como estas")
+print("Bonjour Vscode - wie geht's")
+print("Bonjour Vscode - wie geht's")
 # hello
 
 print("Ceci est une nouvelle ligne")
