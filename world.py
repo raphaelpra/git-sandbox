d5348b4159ef72787c395442bc78614d4dfe5dcf
raphael <raphael.prasquier@imagine-app.fr>
2023-09-21 10:22:11 +0200

    Dernier commit avant la pause

--- world.py
+++ world.py
@@ -1,6 +1,8 @@
-print('hello python')
 
 print("Bonjour tout le monde")
 
 print("Bonjour Vscode - Como estas")
+print("Bonjour Vscode - Como estas")
 # hello
+
+print("Ceci est une nouvelle ligne")
